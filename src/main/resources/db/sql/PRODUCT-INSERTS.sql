f803249bd5fc9e8202d7456735af0318ba85bb60
albertocerqueira <alberto.cerqueira1990@gmail.com>
2016-11-26 15:03:55 -0200

    Commit initial

---
+++ src/main/resources/db/sql/PRODUCT-INSERTS.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCT VALUES (1, 'pen', 'great pen', 2.00);
+INSERT INTO PRODUCT VALUES (2, 'pencil', 'great pencial', 2.00);
+INSERT INTO PRODUCT VALUES (3, 'erase', 'great erase', 1.00);
